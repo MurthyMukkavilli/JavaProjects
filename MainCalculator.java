35acb06574866b0dc7824f4e01f11fd6be55fdea
MurthyMukkavilli <43471200+MurthyMukkavilli@users.noreply.github.com>
2018-09-21 17:19:48 +0530

    FirstJavaProgram

---
+++ MainCalculator.java
@@ -0,0 +1,45 @@
+class Calculator
+{
+		public void add(int a,int b)
+		{
+		int c;
+		c=a+b;
+			System.out.println(c);
+		}
+		public void add(double a,int b)
+		{
+		 double c ;
+		c=a+b;
+			System.out.println(c);
+		}
+		public void add(float a,int b)
+		{
+		 float c=a+b;
+			System.out.println(c);
+		}
+		public void add(double a,double b)
+		{ 
+		double c;
+		c=a+b;
+			System.out.println(c);
+		}	
+			public void add(String a,String b)
+		{ 
+		String c;
+		c=a+b;
+			System.out.println(c);
+		}	
+}
+class MainCalculator
+{
+		public static void main(String[] args)
+		{
+	     	Calculator tol=new Calculator();
+	        tol.add(1,2);
+			tol.add(7.00,5);
+			tol.add(9.00,2);
+			tol.add(2.500,2.978);	
+			tol.add("Murthy","Mukkavilli");			
+			
+		}
+}
